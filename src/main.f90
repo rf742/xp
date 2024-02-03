47028294a86c33266766cb4023051725844134e8
rf <56006623+rf742@users.noreply.github.com>
2024-02-02 23:02:48 -0500

    using fprettify to format code

--- src/main.f90
+++ src/main.f90
@@ -6,42 +6,43 @@ program cross_product
   real :: x1f, y1f, z1f, x2f, y2f, z2f
   real, dimension(3) :: crp
   count = command_argument_count()
-  if (count==6) then
-          call get_command_argument(1, x1)
-          call get_command_argument(2, y1)
-          call get_command_argument(3, z1)
-          call get_command_argument(4, x2)
-          call get_command_argument(5, y2)
-          call get_command_argument(6, z2)
-          read(x1, *) x1f
-          read(y1, *) y1f
-          read(z1, *) z1f
-          read(x2, *) x2f
-          read(y2, *) y2f
-          read(z2, *) z2f
- 
-  else if (count==4) then
-          call get_command_argument(1, x1)
-          call get_command_argument(2, y1)
-          call get_command_argument(3, x2)
-          call get_command_argument(4, y2)
-          read(x1, *) x1f
-          read(y1, *) y1f
-          read(x2, *) x2f
-          read(y2, *) y2f
-          z1f = 0.0
-          z2f = 0.0
-  
+  if (count == 6) then
+
+    call get_command_argument(1, x1)
+    call get_command_argument(2, y1)
+    call get_command_argument(3, z1)
+    call get_command_argument(4, x2)
+    call get_command_argument(5, y2)
+    call get_command_argument(6, z2)
+    read (x1, *) x1f
+    read (y1, *) y1f
+    read (z1, *) z1f
+    read (x2, *) x2f
+    read (y2, *) y2f
+    read (z2, *) z2f
+
+  else if (count == 4) then
+    call get_command_argument(1, x1)
+    call get_command_argument(2, y1)
+    call get_command_argument(3, x2)
+    call get_command_argument(4, y2)
+    read (x1, *) x1f
+    read (y1, *) y1f
+    read (x2, *) x2f
+    read (y2, *) y2f
+    z1f = 0.0
+    z2f = 0.0
+
   else
-          print *, "Invalid number of arguments. Exiting!"
-          stop 1
+    print *, "Invalid number of arguments. Exiting!"
+    stop 1
   end if
-  
+
   print *, count
   ! Read the vector components from the command line
 
   ! Calculate the cross product
-  crp = crossproduct(x1f,y1f,z1f,x2f,y2f,z2f)
+  crp = crossproduct(x1f, y1f, z1f, x2f, y2f, z2f)
 
   ! Print the result
   print *, "The cross product is:", crp(1), crp(2), crp(3)
--- src/xplib.f90
+++ src/xplib.f90
@@ -1,14 +1,14 @@
 module xplib
 contains
-  function crossproduct(x1,y1,z1,x2,y2,z2) result(r)
+  function crossproduct(x1, y1, z1, x2, y2, z2) result(r)
           !! Function to get cross product, given the components of two
           !! Vectors in \(R^3\)
-          real, intent(in)   :: x1,y1,z1,x2,y2,z2
-          real, dimension(3) :: r
-          real              :: crx, cry, crz
-          crx = y1 * z2 - z1 * y2
-          cry = z1 * x2 - x1 * z2
-          crz = x1 * y2 - y1 * x2
-          r = (/ crx, cry, crz /)
+    real, intent(in)   :: x1, y1, z1, x2, y2, z2
+    real, dimension(3) :: r
+    real              :: crx, cry, crz
+    crx = y1*z2 - z1*y2
+    cry = z1*x2 - x1*z2
+    crz = x1*y2 - y1*x2
+    r = (/crx, cry, crz/)
   end function
 end module
