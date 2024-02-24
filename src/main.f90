5c2b4966269a3d754b42e105dc2b1c354ad6c616
rf <56006623+rf742@users.noreply.github.com>
2024-02-24 16:47:43 -0500

    updated cli handling

--- src/main.f90
+++ src/main.f90
@@ -1,34 +1,29 @@
 program cross_product
   use :: xplib
   implicit none
-  integer :: count
-  character(len=32) :: x1, y1, z1, x2, y2, z2
+  integer :: count, n
+  character(len=32),dimension(3) :: va_s, vb_s
   real, dimension(3) :: crp, va, vb
   count = command_argument_count()
   if (count == 6) then
-
-    call get_command_argument(1, x1)
-    call get_command_argument(2, y1)
-    call get_command_argument(3, z1)
-    call get_command_argument(4, x2)
-    call get_command_argument(5, y2)
-    call get_command_argument(6, z2)
-    read (x1, *) va(1)
-    read (y1, *) va(2)
-    read (z1, *) va(3)
-    read (x2, *) vb(1)
-    read (y2, *) vb(2)
-    read (z2, *) vb(3)
+    do n = 1,3
+      call get_command_argument(n,va_s(n))
+      read (va_s(n), *) va(n)
+    end do
+    do n = 1,3
+      call get_command_argument(n+3,vb_s(n))
+      read (vb_s(n), *) vb(n)
+    end do
 
   else if (count == 4) then
-    call get_command_argument(1, x1)
-    call get_command_argument(2, y1)
-    call get_command_argument(3, x2)
-    call get_command_argument(4, y2)
-    read (x1, *) va(1)
-    read (y1, *) va(2)
-    read (x2, *) vb(1)
-    read (y2, *) vb(2)
+    call get_command_argument(1, va_s(1))
+    call get_command_argument(2, va_s(2))
+    call get_command_argument(3, vb_s(1))
+    call get_command_argument(4, vb_s(2))
+    read (va_s(1), *) va(1)
+    read (va_s(2), *) va(2)
+    read (vb_s(1), *) vb(1)
+    read (vb_s(2), *) vb(2)
     va(3) = 0.0
     vb(3) = 0.0
 
@@ -42,7 +37,7 @@ program cross_product
     print *, "    4: B_x"
     print *, "    5: B_y"
     print *, "    6: B_z"
-    print *, "The cross product of the above 2 vectors will be returned"
+    print *, "The cross product of vectors A and B will be returned"
     print *, "If only 4 arguments are given A_z and B_z assumed to be 0"
     stop 1
   end if
